7af55a9ad9b757b24a3276923eb297386d5c4f7e
chiazokam <chiazokamecheta@gmail.com>
2022-02-18 15:02:33 +0100

    feat: Implement the Header

---
+++ src/components/Header/Cart/Cart.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ReactComponent as CartSvg } from '../../../assets/cart.svg';
+import styles from './Cart.module.css'
+
+type CartProps = {
+  withItemCount: Boolean;
+  itemCount: number;
+}
+
+const Cart = ({ withItemCount, itemCount }: CartProps ) => {
+  return (
+    <div className={styles.cart}>
+      <span className={styles.itemCount}>{itemCount}</span>
+      <CartSvg />
+    </div>
+  )
+}
+
+export default Cart;
