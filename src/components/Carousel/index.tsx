e1df851ce1c8432b35a8fac54dd16560277f2ece
chiazokam <chiazokamecheta@gmail.com>
2022-02-21 02:00:09 +0100

    feat: Implement Featured Books Section

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from 'react';
+import { ReactComponent as LeftArrow } from '../../assets/leftArrow.svg';
+import { ReactComponent as RightArrow } from '../../assets/rightArrow.svg';
+import styles from './Carousel.module.css';
+
+type CarouselProps = {
+  children: any;
+}
+
+const Carousel = (props: CarouselProps) => {
+  const { children } = props;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [length, setLength] = useState(children.length);
+  const [touchPosition, setTouchPosition] = useState(null)
+
+  useEffect(() => {
+    setLength(children.length)
+  }, [children])
+
+  const next = () => {
+    if (currentIndex < (length - 1)) {
+      setCurrentIndex(prevState => prevState + 1)
+    }
+  }
+
+  const prev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(prevState => prevState - 1)
+    }
+  }
+
+  const handleTouchStart = (e: any) => {
+    const touchDown = e.touches[0].clientX
+    setTouchPosition(touchDown)
+}
+
+  const handleTouchMove = (e: any) => {
+    const touchDown = touchPosition
+
+    if(touchDown === null) {
+        return
+    }
+
+    const currentTouch = e.touches[0].clientX
+    const diff = touchDown - currentTouch
+
+    if (diff > 5) {
+        next()
+    }
+
+    if (diff < -5) {
+        prev()
+    }
+
+    setTouchPosition(null)
+  }
+
+  return (
+    <div className={styles.carouselContainer}>
+      <div className={styles.carouselWrapper}>
+        {currentIndex > 0 && <button onClick={prev} className={styles.leftArrow}>
+          <LeftArrow />
+        </button>}
+
+        <div
+          className={styles.carouselContentWrapper}
+          onTouchStart={handleTouchStart}
+          onTouchMove={handleTouchMove}
+        >
+          <div className={styles.carouselContent} style={{ transform: `translateX(-${currentIndex * (100 / 6)}%)` }}>
+            {children}
+          </div>
+        </div>
+
+        {currentIndex < (length - 1) && <button onClick={next} className={styles.rightArrow}>
+          <RightArrow />
+        </button>}
+
+      </div>
+    </div>
+  )
+}
+
+export default Carousel;
